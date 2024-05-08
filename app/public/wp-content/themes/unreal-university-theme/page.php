c65722e7add6130301cf5cae04b46ad498a66990
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2024-05-08 11:11:19 +0200

    blog section

--- app/public/wp-content/themes/unreal-university-theme/page.php
+++ app/public/wp-content/themes/unreal-university-theme/page.php
@@ -21,7 +21,7 @@ get_header();
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
             <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true">
-            </i><?php echo get_the_title($theParent); ?></a>
+            </i> Back to <?php echo get_the_title($theParent); ?></a>
             <span class="metabox__main"><?php echo the_title(); ?></span>
           </p>
         </div>
